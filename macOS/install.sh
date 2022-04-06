ab6d323166e6d1413fb7a654f25a97e8736c8f43
João Victor <jvictorsantos852@gmail.com>
2022-04-06 01:23:20 -0300

    Update install.sh

--- macOS/install.sh
+++ macOS/install.sh
@@ -11,3 +11,22 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
 
 # Configure ZSH, oh-my-zsh, plugins and aliases
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+rm -rf ~/.zshrc
+touch ~/.zshrc
+curl https://raw.githubusercontent.com/thejoaov/dotfiles/master/macOS/.zshrc > ~/.zshrc
+
+rm -rf ~/.hyper.js
+curl https://raw.githubusercontent.com/thejoaov/dotfiles/master/macOS/.hyper.js > ~/.hyper.js
+
+rm -rf ~/.gitconfig
+curl https://raw.githubusercontent.com/thejoaov/dotfiles/master/macOS/.gitconfig > ~/.gitconfig
+
+# Install Ruby 2.7.4 with rbenv
+rbenv install 2.7.4
+rbenv global 2.7.4
+rbenv rehash
+
