a377aea07dbd7db6f091e9352d5193572e68c4f3
João Victor <jvictorsantos852@gmail.com>
2024-01-23 17:58:26 -0300

    Update .hyper.js

--- macOS/.hyper.js
+++ macOS/.hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-aura-theme", "hyper-transparent"],
+  plugins: ["hyper-aura-theme"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
